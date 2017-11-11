cbade27a07095484f762117528015712f38cccaa
王晨莉 <462442056@qq.com>
2017-11-11 22:20:02 +0800

    json

--- json&跨域/backend/get_sae.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$content = file_get_contents('http://localhost/9-06/lianxi/js/4-text.txt');
-
-echo $content;
-
--- json&跨域/backend/php/get_sae.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$content = file_get_contents('http://1.class.applinzi.com/test2.php');
-
-echo $content;
-
